efa65958e0678a948db91d41463f2893f15e9a59
barisAdaliDag <barisadali.dag@hotmail.com>
2026-02-09 15:44:40 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'package:cryptoapp/app/const/app_color.dart';
 import 'package:cryptoapp/app/get_it/get_it.dart';
 import 'package:cryptoapp/app/router/app_router.dart';
 import 'package:cryptoapp/app/theme/app_theme.dart';
-import 'package:cryptoapp/app/util/navigation_helper/navigation_helper.dart';
-import 'package:cryptoapp/presentation/market/view/market_list_screen.dart';
 import 'package:cryptoapp/presentation/market/viewmodel/market_list_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  setupGetIt(); // GetIt initialize
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupGetIt();
   runApp(const MyApp());
 }
 
@@ -21,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => getIt<MarketListViewModel>()..loadTickers())],
       child: MaterialApp.router(
-        title: 'Crypto Market',
+        title: 'CryptX',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.darkTheme,
         routerConfig: AppRouter.router,
